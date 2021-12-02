c9ca3c59891ae37ee812bf22d0d1ee5ba3501f29
Jerson Perez <ing.jersonperez@gmail.com>
2021-12-02 18:16:14 -0500

    [Fix] file permisions

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -8,7 +8,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     _mat1 = [row[:] for row in mat1]
     _mat2 = [row[:] for row in mat2]
 
-    if axis == 0:
+    if axis == 0 and len(_mat1) == len(_mat2):
         return _mat1 + _mat2
-    else:
+
+    elif len(_mat1[0]) == len(_mat2[0]):
         return [_mat1[i] + _mat2[i] for i in range(len(_mat1))]
+        
+    else:
+        return None
